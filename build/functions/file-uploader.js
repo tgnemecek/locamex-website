1d449adb336332f5ebcf6fbb7f08af92c75a56ae
thiagonemecek <tgnemecek@gmail.com>
2020-04-24 13:39:16 -0400

    Netlify functions test 2

--- build/functions/file-uploader.js
+++ build/functions/file-uploader.js
@@ -1,9 +1,30 @@
+const AWS = require('aws-sdk')
+
 exports.handler = function(event, context, callback) {
-    console.log('running test-------');
-    console.log("TEST_KEY = " + process.env.TEST_KEY);
-    let key = process.env.TEST_KEY;
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World: " + key
-    });
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      })
+
+    let Bucket = "locamex-website";
+    let Key = "bbb.json";
+
+    s3.putObject({
+        Bucket,
+        Key,
+        ContentType: 'application/json',
+        Body: JSON.stringify({
+            name: "thiago",
+            lastName: "nemecek",
+            type: "person"
+        }),
+    }, (err, data) => {
+        if (err) callback(err);
+        if (data) {
+            callback(null, {
+                statusCode: 200,
+                body: data
+            });
+        }
+    })
 }
