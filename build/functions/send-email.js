05e97998fd6a66c8fbaf39cc14253ea4284a315f
thiagonemecek <tgnemecek@gmail.com>
2020-04-26 22:20:07 -0400

    Email test 5

--- build/functions/send-email.js
+++ build/functions/send-email.js
@@ -2,6 +2,7 @@ var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 
 exports.handler = function(event, context, callback) {
+  try {
     var transporter = nodemailer.createTransport(smtpTransport({
       service: 'gmail',
       auth: {
@@ -9,9 +10,9 @@ exports.handler = function(event, context, callback) {
           pass: process.env.EMAIL_PASSWORD
       }
     }));
-
+  
     var text = 'Email body goes here';
-
+  
     var mailOptions = {
       from: 'tgnemecek@yahoo.com.br',
       to: 'tgnemecek@gmail.com',
@@ -19,23 +20,27 @@ exports.handler = function(event, context, callback) {
       subject: 'Test subject',
       text: text
     };
-
+  
     transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
+      if (error) {
+        const response = {
+          statusCode: 500,
+          body: JSON.stringify({
+            error: error.message,
+          }),
+        };
+        callback(null, response);
+      }
       const response = {
-        statusCode: 500,
+        statusCode: 200,
         body: JSON.stringify({
-          error: error.message,
+          message: `Email processed succesfully!`
         }),
       };
       callback(null, response);
-    }
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Email processed succesfully!`
-      }),
-    };
-    callback(null, response);
-  });
+    });
+  }
+  catch(err) {
+    callback(err);
+  }
 }
